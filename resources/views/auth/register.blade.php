84745229fc94a2dd4cf1f385e91f187dabd97cc1
Paul Colley <drguru.track79@yahoo.co.uk>
2021-05-08 18:49:36 +0100

    fixed errors in login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('home')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -101,7 +101,5 @@
         </div>
       </div>
     </div>
-
-  </div>
-
+</div>
 @endsection
