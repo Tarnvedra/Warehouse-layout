4a4f306bc3b9ac105b7d14f49a7540a16ea32abc
Paul Colley <drguru.track79@yahoo.co.uk>
2024-04-27 15:15:24 +0100

    removal of middleware from controller

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,16 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [PagesController::class ,'index']);
+
+
 Route::get('/dashboard' , [PagesController::class, 'dashboard'])->name('dashboard');
 
+Route::group(['middleware' => ['auth']], function () {
 Route::get('/products/add' , [ProductsController::class, 'addItems'])->name('item.add');
 Route::get('/products/sort' , [ProductsController::class, 'sortItems'])->name('items.sort');
 Route::get('/products/display' , [ProductsController::class, 'displayItems'])->name('items.display');
 Route::get('/products/delete' , [ProductsController::class , 'deleteItems'])->name('item.delete');
 Route::delete('/product/{id}/destroy' , [ProductsController::class, 'destroy']);
 Route::resource('products', 'ProductsController');
+});
 
